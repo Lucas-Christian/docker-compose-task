43d6465bbae53f2380918eb2670455afaa1927d2
Rafael Pinheiro <126368750+HG570@users.noreply.github.com>
2024-09-12 23:37:24 +0000

    Adicionado um comentário legal

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ def get_hit_count():
             retries -= 1
             time.sleep(0.5)
 @app.route('/')
+# A função hello() não diz olá.
 def hello():
     count = get_hit_count()
     return 'Corinthians! I have been seen {} times.\n'.format(count)
